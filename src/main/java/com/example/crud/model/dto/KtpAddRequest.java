2d38b54c7f3ddb7a297db37b6c3c451ab6413ef4
akmaalsyh <taufansyah50@gmail.com>
2026-03-18 15:07:37 +0700

    tambah class KtpDto dan KtpAddRequest untuk pemisahan data

---
+++ src/main/java/com/example/crud/model/dto/KtpAddRequest.java
@@ -0,0 +1,17 @@
+package com.example.crud.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class KtpAddRequest {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
---
+++ src/main/java/com/example/crud/model/dto/KtpDto.java
@@ -0,0 +1,18 @@
+package com.example.crud.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
