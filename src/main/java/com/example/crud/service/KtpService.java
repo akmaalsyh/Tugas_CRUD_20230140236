4231c61b5d167cad5cb280c5e23f86a2e39e9797
akmaalsyh <taufansyah50@gmail.com>
2026-03-18 15:08:16 +0700

    ubah KtpService menjadi interface

--- src/main/java/com/example/crud/service/KtpService.java
+++ src/main/java/com/example/crud/service/KtpService.java
@@ -1,63 +1,14 @@
 package com.example.crud.service;
 
-import com.example.crud.entity.Ktp;
-import com.example.crud.repository.KtpRepository;
-import com.example.crud.util.ValidationUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.example.crud.model.dto.KtpDto;
+import com.example.crud.model.entity.Ktp;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class KtpService {
-
-    @Autowired
-    private KtpRepository repository;
-
-    @Autowired
-    private ValidationUtil validationUtil;
-
-    public List<Ktp> getAllKtp() {
-        return repository.findAll();
-    }
-
-    public Optional<Ktp> getKtpById(Integer id) {
-        return repository.findById(id);
-    }
-
-    public Ktp saveKtp(Ktp ktp) {
-        validationUtil.validate(ktp);
-        if (repository.findByNomorKtp(ktp.getNomorKtp()).isPresent()) {
-            throw new RuntimeException("Nomor KTP sudah terdaftar!");
-        }
-        return repository.save(ktp);
-    }
-
-    public Ktp updateKtp(Integer id, Ktp ktpDetails) {
-        // 1. Validasi dulu data yang dikirim dari Postman
-        validationUtil.validate(ktpDetails);
-
-        // 2. Cari data lama di database
-        Ktp ktp = repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Data KTP tidak ditemukan"));
-
-        // 3. Update field nomorKtp
-        ktp.setNomorKtp(ktpDetails.getNomorKtp());
-
-        // 4. Update field lainnya
-        ktp.setNamaLengkap(ktpDetails.getNamaLengkap());
-        ktp.setAlamat(ktpDetails.getAlamat());
-        ktp.setTanggalLahir(ktpDetails.getTanggalLahir());
-        ktp.setJenisKelamin(ktpDetails.getJenisKelamin());
-
-        // 5. Simpan perubahan ke database
-        return repository.save(ktp);
-    }
-
-    public void deleteKtp(Integer id) {
-        if (!repository.existsById(id)) {
-            throw new RuntimeException("ID tidak ditemukan");
-        }
-        repository.deleteById(id);
-    }
+public interface KtpService {
+    List<Ktp> getAllKtp();
+    Optional<Ktp> getKtpById(Integer id);
+    Ktp saveKtp(Ktp ktp);
+    Ktp updateKtp(Integer id, Ktp ktpDetails);
+    void deleteKtp(Integer id);
 }
